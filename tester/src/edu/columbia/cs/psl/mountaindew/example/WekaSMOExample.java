e1a58fd65fac2bd868a5753033b5a788667b7045
Mike <fs2455@columbia.edu>
2013-07-25 10:51:43 -0400

    SOM in Weka

---
+++ tester/src/edu/columbia/cs/psl/mountaindew/example/WekaSMOExample.java
@@ -0,0 +1,81 @@
+package edu.columbia.cs.psl.mountaindew.example;
+
+import java.io.File;
+import java.io.IOException;
+
+import weka.classifiers.Evaluation;
+import weka.classifiers.functions.SMO;
+import weka.core.Instance;
+import weka.core.Instances;
+import weka.core.converters.ArffLoader;
+
+public class WekaSMOExample {
+	
+	public Instances loadData(String dataPath) {
+		ArffLoader dataLoader = new ArffLoader();
+		try {
+			dataLoader.setFile(new File(dataPath));
+			
+			Instances data = dataLoader.getDataSet();
+			data.setClassIndex(data.numAttributes() - 1);
+			
+			return data;
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+		
+		return null;
+	}
+	
+	public SMO trainSMOModel(Instances data) {
+		SMO smoDriver = new SMO();
+		String[] options = new String[]{"-t"};
+		try {
+			smoDriver.setOptions(options);
+			smoDriver.buildClassifier(data);
+			
+			return smoDriver;
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+		return null;
+	}
+	
+	public void evalModel(SMO smoDriver, Instances data) {
+		Evaluation eval = null;
+		try {
+			eval = new Evaluation(data);
+			eval.evaluateModel(smoDriver, data);
+			System.out.println(eval.toSummaryString("Result\n", false));
+		} catch(Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+
+	/**
+	 * @param args
+	 * @throws Exception 
+	 */
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		String dataPath = "data/iris.arff";
+		
+		WekaSMOExample smoEx = new WekaSMOExample();
+		Instances data = smoEx.loadData(dataPath);
+		
+		if (data == null) {
+			System.err.println("Load no data");
+			return ;
+		}
+		
+		SMO smo = smoEx.trainSMOModel(data);
+		
+		if (smo == null) {
+			System.err.println("Fail to train model");
+			return ;
+		}
+		
+		smoEx.evalModel(smo, data);
+	}
+
+}
